5a54be44937b35e888b33ec26029c176c46b1785
bhuwan <bkspoudel@gmail.com>
2023-08-09 19:27:45 +0200

    update express validator middleware to the userroute

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const { check } = require('express-validator');
+
 const userController = require('../controller/users')
+
 
 const route = express.Router();
 
-route.post('/', userController.createUsers);
+route.post(
+    '/signup', 
+    [
+        check('name')
+            .not()
+            .isEmpty(),
+        check('email')
+            .normalizeEmail() // Test@test.com => test@test.com
+            .isEmail(),
+        check('password').isLength({ min: 6})
+    ],
+    userController.createUsers);
 route.get('/', userController.getAllUsers);
 
 module.exports = route;

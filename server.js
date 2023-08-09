a485c3946524a6393fa9abf55736bedec728437f
bhuwan <bkspoudel@gmail.com>
2023-08-09 22:47:47 +0200

    add client error

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const UserRoute = require('./routes/users');
 const PlaceRoute = require('./routes/place');
-const connectDb = require('./config/db')
+const connectDb = require('./config/db');
+const HttpError = require('./models/http-errors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api/users', UserRoute);
 app.use('/api/places', PlaceRoute);
+
+app.use((req, res, next)=>{
+    const error = new HttpError('Could not find this route', 404);
+    throw error;                  // return next(error);
+})
 
 app.use((error, req, res, next) => {
     if(res.headerSent){
